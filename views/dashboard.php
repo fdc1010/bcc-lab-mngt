db83ae880238b9365e5227d175aee8013ad564de
fdc1010 <franklincaruana@gmail.com>
2025-02-03 00:01:57 +0800

    chore: add some items to dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -4,8 +4,17 @@
 	include './layouts/side_bar.php';
 
 	include './components/tbl_dashboard.php';
+	
+	include './components/tbl_items.php';
+
+	include './components/tbl_borrow.php';
+
+	include './components/tbl_inventory.php';
+
+	include './components/ghp_report.php';
 
 	include './layouts/footer.php';
 
 	include './components/tbl_dashboard_js.php';
+	include './components/ghp_report_js.php';
 ?>
